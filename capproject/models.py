9011274ab713db1950e0a9a0fcbb6945a672d5c0
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-25 16:21:41 +0300

    migrations

--- capproject/models.py
+++ capproject/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
   image = models.ImageField(blank=True, upload_to='product_images')
   price = models.DecimalField(max_digits=6, decimal_places=2)
   seller = models.TextField()
-  platform = models.TextField()
+  platform_site = models.URLField(max_length=100, blank=True)
 
   def __str__(self):
     return self.name 
